4dc64fed78e3482c2dc45fea5625fa43e0fb1ff2
Arjanit07 <165574844+Arjanit07@users.noreply.github.com>
2024-05-27 20:10:00 +0200

    Update populate.py
    final

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -34,7 +34,7 @@ def initiate():
        "year": 2023,
        "car_make": car_make_instances[1]},
       {"name": "A4", "type": "SUV", "year": 2023,
-       "car_make":car_make_instances[2]},
+       "car_make": car_make_instances[2]},
       {"name": "A5", "type": "SUV",
        "year": 2023,
        "car_make": car_make_instances[2]},
@@ -44,7 +44,7 @@ def initiate():
        "type": "SUV", "year": 2023,
        "car_make": car_make_instances[3]},
       {"name": "Carnival", "type": "SUV",
-       "year": 2023, "car_make":car_make_instances[3]},
+       "year": 2023, "car_make": car_make_instances[3]},
       {"name": "Cerato", "type": "Sedan", "year": 2023,
        "car_make": car_make_instances[3]},
       {"name": "Corolla", "type": "Sedan",
@@ -52,11 +52,10 @@ def initiate():
        "car_make": car_make_instances[4]},
       {"name": "Camry", "type": "Sedan",
        "year": 2023,
-       "car_make":car_make_instances[4]},
+       "car_make": car_make_instances[4]},
       {"name": "Kluger", "type": "SUV",
        "year": 2023,
        "car_make": car_make_instances[4]}
-        # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
